b707c4237e1f48fd5dd1b9f977ff2ecd1097d7af
lapisknight <39871632+lapisknight@users.noreply.github.com>
2018-06-02 22:45:25 -0700

    Update usdandb.js

--- usdandb.js
+++ usdandb.js
@@ -1,5 +1,9 @@
 /**
-The data source for obtaining information from discogs.com.
+The data source for obtaining information from https://api.nal.usda.gov. Code modified from: 
+https://github.com/mementodatabase/scripts/blob/master/data-sources/discogs.js
+Not changing all of the comments and variable names because I'm lazy.
+
+
 @param {string} apiKey - Consumer key.
 @param {string} apiSecret - Consumer secret. 
 @param {string} type - One of release, master, artist.
@@ -10,10 +14,8 @@ var discogs = new Discogs("Consumer key" ,"Consumer secret" , "release" );
 var r = discogs.search(query);
 result( r , function(id) { return discogs.extra(id);});
 */
-function Discogs (apiKey , apiSecret, type) {
+function Discogs (apiKey ) {
     this.apiKey = apiKey;
-    this.apiSecret = apiSecret;
-    this.type = type;
 }
 
 
@@ -22,7 +24,7 @@ Issue a search query to Discogs database.
 @param {string} query - Search query.
 */
 Discogs.prototype.search = function(query) {
-  var result = http().get("https://api.discogs.com/database/search?q=" + encodeURIComponent(query) + "&key=" + this.apiKey + "&secret=" + this.apiSecret + "&type=" + this.type);
+  var result = http().get("https://api.nal.usda.gov/ndb/search/?format=json&q=" + encodeURIComponent(query) + "&sort=n&max=25&offset=0&api_key=" + this.apiKey);
   var json = JSON.parse(result.body);
   return json.results;  
 }
@@ -32,7 +34,7 @@ Issue a search query to Discogs database.
 @param {string} code - Search barcodes.
 */
 Discogs.prototype.barcode = function(code) {
-  var result = http().get("https://api.discogs.com/database/search?barcode=" + encodeURIComponent(code) + "&key=" + this.apiKey + "&secret=" + this.apiSecret + "&type=" + this.type);
+  var result = http().get("https://api.nal.usda.gov/ndb/reports/?ndbno=" + encodeURIComponent(code) + "&type=b&format=json&api_key=" + this.apiKey);
   var json = JSON.parse(result.body);
   return json.results;  
 }
@@ -41,19 +43,7 @@ Discogs.prototype.barcode = function(code) {
 @param {string} id - The resource identifier.
 */
 Discogs.prototype.extra = function(id) {
-    var resultJson = http().get("https://api.discogs.com/" + this.type + "s/" + id + "?key=" + this.apiKey + "&secret=" + this.apiSecret);
+    var resultJson = http().get("https://api.nal.usda.gov/ndb/reports/?ndbno=" + id + "&type=b&format=json&api_key=" + this.apiKey);
     var result = JSON.parse(resultJson.body); 
-    if (result.images !== undefined) 
-        result['images'] = result.images.map(function(e) { return e.uri; }).join(); 
-    if (result.videos !== undefined) 
-        result['videos'] = result.videos.map(function(e) { return e.uri; }).join();     
-    if (result.artists !== undefined)
-        result['artists'] = result.artists.map(function(e) { return e.name; }).join();   
-    if (result.tracklist !== undefined)  
-        result['tracklist'] = result.tracklist.map(function(e) { return e.position + ". " + e.title + " " + e.duration; }).join("\n");     
-    if (result.styles !== undefined)  
-        result['styles'] = result.styles.join();     
-    if (result.genres !== undefined)
-        result['genres'] = result.genres.join();        
     return result;
 }
